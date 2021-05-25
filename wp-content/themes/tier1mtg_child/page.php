324dd461e977d304bb18952a516ba884e95e6573
pern7295 <pern7295@stud.kea.dk>
2021-05-25 13:01:15 +0200

    prøve

---
+++ wp-content/themes/tier1mtg_child/page.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package GeneratePress
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+get_header(); ?>
+
+    <div id="primary" <?php generate_do_element_classes( 'content' ); ?>>
+        <main id="main" <?php generate_do_element_classes( 'main' ); ?>> hej
+        </main>
+    </div>
+
+    <?php
+	/**
+	 * generate_after_primary_content_area hook.
+	 *
+	 * @since 2.0
+	 */
+	do_action( 'generate_after_primary_content_area' );
+
+	generate_construct_sidebars();
+
+	get_footer();
