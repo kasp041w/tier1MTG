5c8df21f41b4dcb809c19f6419d509560836d53d
sabi4826 <sabi4826@stud.kea.dk>
2021-05-26 14:28:20 +0200

    first css baggrundsfarver og farver på tekst

--- wp-content/themes/tier1mtg_child/page-commander.php
+++ wp-content/themes/tier1mtg_child/page-commander.php
@@ -7,20 +7,48 @@ if ( ! defined( 'ABSPATH' ) ) {
 get_header(); ?>
 
 <style>
+	/*SEKTIONER OG FARVER*/
 
+	.splash {}
+
+	#top_sektion {
+		background-color: #F5F5F5;
+	}
+
+	#top_sektion h2,
+	p {
+		color: #272727;
+	}
+
+	#cardmarket_sektion {
+		background-color: #272727;
+	}
+
+	#cardmarket_sektion h2 {
+		color: #F5F5F5;
+	}
+
+	#cardmarket_sektion p {
+		color: #AD9261;
+	}
+
+	#kort_sektion,
+	#event_sektion,
+	#artikler {
+		background-color: #F7F6E9;
+		color: #272727;
+	}
 
 </style>
 
 <div id="primary" <?php generate_do_element_classes( 'content' ); ?>>
 	<main id="main" <?php generate_do_element_classes( 'main' ); ?>>
+		<img src="http://kasperdyhl.dk/tier1mtg/wp-content/uploads/2021/05/commander_splach.png" alt="commander splash billede" class="splash">
 		<section id="top_sektion">
-			<img src="" alt="splash_billede" class="">
 			<h1>Commander</h1>
-			<button></button>
 			<div class="top_col_1">
-				<img src="" alt="">
 				<h2>Hvad er Commander?</h2>
-				<p>Tekst her om Commander-formatet</p>
+				<p>Tekst her om Commander-formatet. Blabjsndlks ifrdn ljiejr je rjwtnntndjfn erj shekj kje bkj bjkbgkj vkd kjdkj kjhekjr kjsh kjheskjt hhg kjhgkjh kjhkj hfkhdgl</p>
 			</div>
 			<div class="top_col_2">
 				<video src=""></video>
@@ -29,19 +57,23 @@ get_header(); ?>
 		<section id="kort_sektion">
 			<h2>Kort du ikke må gå glip af</h2>
 			<a href="">Se alle single cards</a>
+
 		</section>
-		<section id="ugens_tip_sektion">
+		<section id="tip_sektion">
+			<img src="http://kasperdyhl.dk/tier1mtg/wp-content/uploads/2021/05/commander_baggrund.png" alt="grafik" class="tip_baggrund">
 			<div class="tip_col_1">
 				<h2>Se Ugens Tip</h2>
 				<p>Tekst her om ugens tip</p>
-				<button></button>
+				<button>
+					<img src="http://kasperdyhl.dk/tier1mtg/wp-content/uploads/2021/05/youtube_knap.png" alt="play knap">
+				</button>
 				<p>Se vores Commander-playliste på Youtube</p>
 			</div>
 			<div class="tip_col_2">
 				<video src=""></video>
 			</div>
 		</section>
-		<section>
+		<section id="event_sektion">
 			<h2>Skal du være med næste gang?</h2>
 			<a href="">Se alle vores Commander-events</a>
 			<!--			billedgalleri ind her - hvordan? -->
@@ -51,7 +83,7 @@ get_header(); ?>
 			<p>Hold dig opdateret på det nyeste inden for Commander-formatet</p>
 			<!--			Events ind her - hvordan?-->
 		</section>
-		<section>
+		<section id="cardmarket_sektion">
 			<h2>Vi er også på Cardmarket</h2>
 			<p>Tekst om Cardmarket her</p>
 			<button>Læs mere</button>
