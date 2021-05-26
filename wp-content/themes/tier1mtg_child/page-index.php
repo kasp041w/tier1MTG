894bc368932ad139818c6a242fd56b5f4d96a28e
Ida-23 <idax7887@stud.kea.dk>
2021-05-26 13:27:35 +0200

    forside

--- wp-content/themes/tier1mtg_child/page-index.php
+++ wp-content/themes/tier1mtg_child/page-index.php
@@ -138,6 +138,35 @@ get_header(); ?>
 
         }
 
+        function visNewSingleKort() {
+            console.log("visNewPodcast");
+
+            //Genererer et nyt array af tilfældige objekter fra det komplette array
+            const other1 = newSingleKort[Math.floor(Math.random() * newSingleKort.length)];
+            const randomSingleKort = [other1];
+            console.log(randomSingleKort);
+
+            randomSingleKort.forEach(single => {
+                //Definerer konstanter til senere brug i kloningen af template
+                const template = document.querySelector("template");
+
+                const container = document.querySelector(".new_single_kort:nth-child(2)");
+
+                const klon = template.cloneNode(true).content; //Her klones template og udfyldes med data fra de tilfældige objekter
+                klon.querySelector(".billede").src = single.billede.guid;
+                klon.querySelector(".titel").innerHTML = single.title.rendered;
+                klon.querySelector(".pris").innerHTML = `${"Fra "}` + single.pris;
+
+                // eventlisteners på hver enkelt artikel
+                klon.querySelector(".se_kort_knap").addEventListener("click", () => {
+                    location.href = single.link;
+                })
+
+                container.appendChild(klon);
+            })
+
+        }
+
     </script>
 
 
