8ba01cff9d0ffda6e85c2a339da03183686672b2
kasperdyhl <dyhl.bm@gmail.com>
2021-05-28 13:59:01 +0200

    kontakt 122vh

--- wp-content/themes/tier1mtg_child/page-om-os.php
+++ wp-content/themes/tier1mtg_child/page-om-os.php
@@ -80,7 +80,7 @@ get_header(); ?>
                 background-image: url(http://kasperdyhl.dk/tier1mtg/wp-content/uploads/2021/05/kontakt_baggrund.png);
                 background-size: cover;
                 width: 100%;
-                height: auto;
+                height: 122;
                 text-align: center;
                 padding-top: 130px;
                 padding-bottom: 110px;
