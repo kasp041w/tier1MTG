a220055470bc264136305fd33ebc24d657ecbf67
sabi4826 <sabi4826@stud.kea.dk>
2021-05-28 15:59:32 +0200

    SINGLES IND FRA FORSIDEN med javascript

--- wp-content/themes/tier1mtg_child/page-commander.php
+++ wp-content/themes/tier1mtg_child/page-commander.php
@@ -37,6 +37,10 @@ get_header(); ?>
 		padding-right: 2rem;
 	}
 
+	h1 {
+		margin-bottom: 20px;
+	}
+
 	/*FARVER OG DIVERSE*/
 
 	#commander_top_sektion {
@@ -97,9 +101,20 @@ get_header(); ?>
 	article:hover .baggrund_kort {
 		height: 0%;
 		padding: 0px 20px 0px 20px;
-
 	}
 
+	/*SLIDER OPSÆTNING TIL MOBIL OG DESKTOP*/
+	.forside_figure_ny {
+		cursor: pointer;
+	}
+
+	.forside_image-container .forside_figure_ny {
+		flex-basis: 90%;
+		flex-shrink: 0;
+		scroll-snap-align: center;
+	}
+
+	/*UGENS TIP-SEKTION*/
 	#commander_tip_sektion {
 		background-image: url("http://kasperdyhl.dk/tier1mtg/wp-content/uploads/2021/05/commander_baggrund.png");
 		background-size: cover;
@@ -385,6 +400,156 @@ get_header(); ?>
 		</section>
 
 	</main>
+
+	<!--SKABELON TIL SINGLES - KORT DU IKKE MÅ GÅ GLIP AF-->
+	<template>
+		<article>
+			<div class="billed_placering">
+				<img src="" alt="" class="billede">
+				<button class="se_kort_knap knapper_dark">SE KORT</button>
+			</div>
+			<div class="baggrund_kort">
+				<h2 class="titel_alternativ"></h2>
+				<h3 class="pris_alternativ"></h3>
+			</div>
+
+		</article>
+	</template>
+
+	<!--JAVASCRIPT - MATH RANDOM TIL SINGLES-->
+	<script>
+		let singles;
+
+		// url til wp rest api/database
+		const url = "http://kasperdyhl.dk/tier1mtg/wp-json/wp/v2/single?per_page=100";
+
+		async function loadJson() {
+			const JsonData = await fetch(url);
+			newSingleKort = await JsonData.json();
+			console.log("loadJson", newSingleKort);
+			visNewSingleKort();
+			visNewSingleKort1();
+			visNewSingleKort2();
+			visNewSingleKort3();
+		}
+
+		loadJson();
+
+		//Her i funktioen genereres tre tilfeldeig singles og sættes ind i HTML under sektionen Kort du ikke må gå glip af
+		function visNewSingleKort() {
+			console.log("visNewSingleKort");
+
+			//Genererer et nyt array af tilfældige objekter fra det komplette array
+			const other1 = newSingleKort[Math.floor(Math.random() * newSingleKort.length)];
+			const randomSingleKort = [other1];
+			console.log(randomSingleKort);
+
+			randomSingleKort.forEach(single => {
+				//Definerer konstanter til senere brug i kloningen af template
+				const template = document.querySelector("template");
+				const container = document.querySelector(".new_single_kort");
+
+
+				const klon = template.cloneNode(true).content; //Her klones template og udfyldes med data fra de tilfældige objekter
+				klon.querySelector(".billede").src = single.billede.guid;
+				klon.querySelector(".titel_alternativ").innerHTML = single.title.rendered;
+				klon.querySelector(".pris_alternativ").innerHTML = `${"Fra "}` + single.pris;
+
+				// eventlisteners på hver enkelt artikel
+				klon.querySelector(".se_kort_knap").addEventListener("click", () => {
+					location.href = single.link;
+				})
+
+				container.appendChild(klon);
+			})
+		}
+
+		function visNewSingleKort1() {
+			console.log("visNewSingleKort1");
+
+			//Genererer et nyt array af tilfældige objekter fra det komplette array
+			const other1 = newSingleKort[Math.floor(Math.random() * newSingleKort.length)];
+			const randomSingleKort = [other1];
+			console.log(randomSingleKort);
+
+			randomSingleKort.forEach(single => {
+				//Definerer konstanter til senere brug i kloningen af template
+				const template = document.querySelector("template");
+				const container = document.querySelector(".new_single_kort1");
+
+
+				const klon = template.cloneNode(true).content; //Her klones template og udfyldes med data fra de tilfældige objekter
+				klon.querySelector(".billede").src = single.billede.guid;
+				klon.querySelector(".titel_alternativ").innerHTML = single.title.rendered;
+				klon.querySelector(".pris_alternativ").innerHTML = `${"Fra "}` + single.pris;
+
+				// eventlisteners på hver enkelt artikel
+				klon.querySelector(".se_kort_knap").addEventListener("click", () => {
+					location.href = single.link;
+				})
+
+				container.appendChild(klon);
+			})
+		}
+
+		function visNewSingleKort2() {
+			console.log("visNewSingleKort2");
+
+			//Genererer et nyt array af tilfældige objekter fra det komplette array
+			const other1 = newSingleKort[Math.floor(Math.random() * newSingleKort.length)];
+			const randomSingleKort = [other1];
+			console.log(randomSingleKort);
+
+			randomSingleKort.forEach(single => {
+				//Definerer konstanter til senere brug i kloningen af template
+				const template = document.querySelector("template");
+				const container = document.querySelector(".new_single_kort2");
+
+
+				const klon = template.cloneNode(true).content; //Her klones template og udfyldes med data fra de tilfældige objekter
+				klon.querySelector(".billede").src = single.billede.guid;
+				klon.querySelector(".titel_alternativ").innerHTML = single.title.rendered;
+				klon.querySelector(".pris_alternativ").innerHTML = `${"Fra "}` + single.pris;
+
+				// eventlisteners på hver enkelt artikel
+				klon.querySelector(".se_kort_knap").addEventListener("click", () => {
+					location.href = single.link;
+				})
+
+				container.appendChild(klon);
+			})
+		}
+
+		function visNewSingleKort3() {
+			console.log("visNewSingleKort3");
+
+			//Genererer et nyt array af tilfældige objekter fra det komplette array
+			const other1 = newSingleKort[Math.floor(Math.random() * newSingleKort.length)];
+			const randomSingleKort = [other1];
+			console.log(randomSingleKort);
+
+			randomSingleKort.forEach(single => {
+				//Definerer konstanter til senere brug i kloningen af template
+				const template = document.querySelector("template");
+				const container = document.querySelector(".new_single_kort3");
+
+
+				const klon = template.cloneNode(true).content; //Her klones template og udfyldes med data fra de tilfældige objekter
+				klon.querySelector(".billede").src = single.billede.guid;
+				klon.querySelector(".titel_alternativ").innerHTML = single.title.rendered;
+				klon.querySelector(".pris_alternativ").innerHTML = `${"Fra "}` + single.pris;
+
+				// eventlisteners på hver enkelt artikel
+				klon.querySelector(".se_kort_knap").addEventListener("click", () => {
+					location.href = single.link;
+				})
+
+				container.appendChild(klon);
+			})
+		}
+
+	</script>
+
 </div>
 
 <?php
