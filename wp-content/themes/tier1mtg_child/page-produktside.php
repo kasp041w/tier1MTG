ed7a4586acee475654e263e7e2172ec6a46ebcdf
Ida-23 <idax7887@stud.kea.dk>
2021-05-29 13:10:33 +0200

    prudukt

--- wp-content/themes/tier1mtg_child/page-produktside.php
+++ wp-content/themes/tier1mtg_child/page-produktside.php
@@ -417,7 +417,7 @@ get_header(); ?>
 
         <section id="nav-desk">
             <nav class="filtrering-desk">
-                <button class="filter_knapper knapper_light" data-single="alle">Alle</button>
+                <button class="filter_knapper katagori_knapper_produkt" data-single="alle">Alle</button>
             </nav>
         </section>
 
