5b148ccca02d3eb7013eb27cac2dd4a3c7291773
Ida-23 <idax7887@stud.kea.dk>
2021-05-31 19:53:21 +0200

    commander

--- wp-content/themes/tier1mtg_child/page-om-os.php
+++ wp-content/themes/tier1mtg_child/page-om-os.php
@@ -162,7 +162,7 @@ get_header(); ?>
             @media (min-width: 900px) {
 
                 #omOs {
-                    margin: 16vw auto 2vw auto;
+                    margin: 5vw auto 2vw auto;
                 }
 
                 .omOsTekstBoks {
