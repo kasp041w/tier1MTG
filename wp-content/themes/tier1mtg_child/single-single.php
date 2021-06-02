220f61c4f7ac923cb4bcbc22cf034f606bc70654
Ida-23 <idax7887@stud.kea.dk>
2021-06-02 16:43:56 +0200

    singles

--- wp-content/themes/tier1mtg_child/single-single.php
+++ wp-content/themes/tier1mtg_child/single-single.php
@@ -84,13 +84,25 @@ get_header(); ?>
     }
 
     .single_kurv {
+        background-image: url(http://kasperdyhl.dk/tier1mtg/wp-content/uploads/2021/06/kurv_knap1.png) no-repeat;
+    }
+
+    .single_kurv:hover {
+        background-image: url(http://kasperdyhl.dk/tier1mtg/wp-content/uploads/2021/06/kurv_knap_2.png) no-repeat;
+    }
+
+
+    /*
+    .single_kurv {
         width: 8rem;
     }
 
     .single_kurv:hover {
         cursor: pointer;
         transform: scale(1.1);
+
     }
+*/
 
     .single_fragt {
         display: grid;
@@ -352,7 +364,8 @@ get_header(); ?>
                 <h2 class="titel"></h2>
                 <p class="pris"></p>
                 <p class="beskrivelse"></p>
-                <img class="single_kurv" src="http://kasperdyhl.dk/tier1mtg/wp-content/uploads/2021/05/kurv.png" alt="Kurv-ikon">
+                <div class="single_kurv"></div>
+                <!--                <img class="single_kurv" src="http://kasperdyhl.dk/tier1mtg/wp-content/uploads/2021/06/kurv_knap_2.png" alt="Kurv-ikon">-->
                 <p class="lagertal"></p>
                 <div class="single_fragt">
                     <img src="http://kasperdyhl.dk/tier1mtg/wp-content/uploads/2021/05/fragt.png" alt="Fragt-ikon">
